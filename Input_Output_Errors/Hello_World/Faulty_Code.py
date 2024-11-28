37c78e5bd2a14ee7e16b1db4c8c7b782ef60e98c
mur3l <tippitauno@gmail.com>
2024-11-28 17:25:17 +0200

    Classwork "Hello_World" code
    Add "Faulty_Code.py" file
    Exercise fault code messages

---
+++ Input_Output_Errors/Hello_World/Faulty_Code.py
@@ -0,0 +1,30 @@
+#World = ""
+from tkinter import Variable
+
+#print("Hello ",World) World = WW sunnib viga ignoreerima
+
+#print("Hello World) Jutumärgid lõpetamata
+
+# print ("Hello World") Taande viga (Tühik koodi ees)
+
+# if __name__ == '__main__':
+# print("Hello World") Taande viga (automaatsed tühikud eemaldatud eest)
+
+# print(undeclared_variable + 10) (Viitamine defineerimata "Name error" -> undeclared_variable + 10)
+
+#variable = "Viis"
+#print(variable + 10) (Sõne saab ainult sõnega ühendada "TypeError")
+
+# variable = "Viis"
+# print(10 + variable) (Sisuliselt sama viga kuid erineb alustatavast käsust "TypeError")
+
+#def first():
+#second(a:"The output is called", b:"Stack")
+#first()
+
+
+
+
+
+
+
