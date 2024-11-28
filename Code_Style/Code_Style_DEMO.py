314848308a93d2327066dbf4b413ac966ebf8084
mur3l <tippitauno@gmail.com>
2024-11-28 18:03:27 +0200

    New "Code_Style_DEMO.py" Exercise

---
+++ Code_Style/Code_Style_DEMO.py
@@ -0,0 +1,37 @@
+"""Demonstrate how codestyle should be applied."""
+
+#Parem klõps "third (muudetud "output_int_value_of)" -> "Show Context Action" -> "Insert a documentation string stub"
+def output_int_value_of(a):
+    """
+    Convert parameter into integer and print its value
+
+    :param a: integer string that should be printed
+    """
+    print(int(a))
+
+
+def combine(a, b):
+    """
+    Add two parameters together.
+
+    :param a: first value to add
+    :param b: secound value to add
+    """
+    c = a + b
+    print(c)
+
+    third(c)
+
+
+def first()
+
+    combine(a:"The output is called ", b:"'Stack Trace'")
+
+
+def start_program()
+        """Start the demo program."""
+        #Calling the second funtion
+        combine(a:"The output is called ", b:"'Stack Trace'")
+
+
+start_program()
